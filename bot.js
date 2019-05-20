22c38913e0c659d42dfe66dbe41026274a11a5dd
zeiadamr <45317405+zeiadamr@users.noreply.github.com>
2019-05-20 06:44:52 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Iam Zeiadamr`,"http://twitch.tv/Dream")
+client.user.setGame(`Iam Alan`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
